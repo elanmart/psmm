d7434a00005415fb01aaee4e38b08458e5815f1f
Marcin Elantkowski <marcin.elantkowski@gmail.com>
2018-04-26 12:47:20 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ setup(
     name='psmm',
     version='0.1',
     packages=find_packages(),
-    url='',
+    url='https://github.com/elanmart/psmm/',
     license='MIT',
-    author='elan',
+    author='Marcin Elantkowski',
     author_email='marcin.elantkowski@gmail.com',
     description='Implementation of Pointer Sentinel Mixture Model'
 )
